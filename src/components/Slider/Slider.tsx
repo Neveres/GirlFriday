e01d11c62181e6ca06d91325c08a9e5091b44f44
Leo <lechewu@gmail.com>
2023-01-24 00:05:10 +0800

    feat: adjust scale

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Slider as MuiSlider, ThemeProvider } from '@mui/material'
 import { SliderUnstyledOwnProps } from '@mui/base/SliderUnstyled'
 import { theme } from './theme'
@@ -10,27 +10,45 @@ interface ISlider {
 }
 
 const values = [3, 6, 9, 12, 15, 50]
-const marks = values.map((value) => ({
-  value,
+const interval = 100 / values.length
+const marks = values.map((value, index) => ({
+  value: interval * index,
   label: value,
+  scaledValue: value,
 }))
+const max = interval * (values.length - 1)
+const scale = (value: number) => {
+  const transformedMark = marks.find((mark) => mark.value === value)
+  if (transformedMark) {
+    return transformedMark?.scaledValue as number
+  } else {
+    const target = marks.find((mark) => mark.scaledValue === value)
+    return target?.value as number
+  }
+}
 
 const Slider: React.FC<ISlider> = ({ value, setValue, isMobile }) => {
   const onChange: SliderUnstyledOwnProps['onChange'] = (event, newValue) => {
-    setValue(newValue as number)
+    const index = (newValue as number) / interval
+    setValue(marks[index].scaledValue)
   }
+
+  const trasnformedValue = useMemo(
+    () => marks.find((mark) => mark.scaledValue === value)?.value,
+    [value],
+  )
 
   return (
     <ThemeProvider theme={isMobile ? theme.mobile : theme.desktop}>
       <MuiSlider
-        defaultValue={value}
-        value={value}
+        value={trasnformedValue}
         valueLabelDisplay="auto"
         marks={marks}
         step={null}
-        max={values[values.length - 1]}
-        min={values[0]}
+        max={max}
+        min={0}
         onChange={onChange}
+        scale={scale}
       />
     </ThemeProvider>
   )
