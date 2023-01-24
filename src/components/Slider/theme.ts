a0f31e5b5dcf10882f8cd98a0072b2779478a001
Leo <lechewu@gmail.com>
2023-01-24 23:09:37 +0800

    style(Slider): stick to mock

--- src/components/Slider/theme.ts
+++ src/components/Slider/theme.ts
@@ -3,8 +3,9 @@ import { colors } from 'src/GlobalCss'
 
 const styleOverrides = {
   root: {
-    width: '718px',
+    width: '724px',
     '& .MuiSlider-markLabel': {
+      top: '34px',
       color: colors.textWhite,
       fontWeight: 500,
       fontSize: '16px',
@@ -14,11 +15,19 @@ const styleOverrides = {
     '& .MuiSlider-markLabelActive': {
       opacity: 1,
     },
+    '& .MuiSlider-mark': {
+      backgroundColor: 'initial',
+    },
+    '& .MuiSlider-rail': {
+      height: '8px',
+    },
   },
   track: {
     background: 'linear-gradient(270deg, #ffd25f 0.13%, #ff5c01 100%)',
   },
   thumb: {
+    width: '25px',
+    height: '25px',
     background: '#1B1B1B',
     border: '6px solid #FFD05D',
   },
