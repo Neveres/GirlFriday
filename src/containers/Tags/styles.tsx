828e8e02da938528e33096310a37a90005ef8d16
Leo <lechewu@gmail.com>
2023-01-26 21:15:57 +0800

    style(Tags): stick to mock

--- src/containers/Tags/styles.tsx
+++ src/containers/Tags/styles.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react'
-import { colors, fontWeight } from 'src/GlobalCss'
+import { colors, fontWeight, fontSize } from 'src/GlobalCss'
 
 export const tagsContainer = {
   desktop: css`
@@ -76,40 +76,46 @@ export const tagsContainer = {
     margin-left: 20px;
 
     .tags-header {
-      display: flex;
+      position: relative;
       align-items: center;
       width: 375px;
       height: 70px;
       margin-bottom: 20px;
 
       svg {
-        width: 40px;
-        height: 40px;
+        position: absolute;
+        top: 15px;
+        left: -10px;
+        width: 43px;
+        height: 43px;
         margin-right: 20px;
         cursor: pointer;
       }
 
       span {
+        position: absolute;
+        top: 17px;
+        left: 38px;
         font-weight: ${fontWeight.lighter};
-        font-size: 30px;
+        font-size: ${fontSize.xxLarge};
         line-height: 150%;
-        letter-spacing: 0.25px;
       }
     }
 
     .page-title {
       font-weight: ${fontWeight.lighter};
-      font-size: 30px;
+      font-size: ${fontSize.xxLarge};
       line-height: 150%;
-      margin: 0 0 20px 0;
+      margin: 0 0 24px 0;
     }
 
     .tag-container {
       width: 150px;
       height: 199px;
       display: inline-block;
+      margin-left: 5px;
       margin-right: 19px;
-      margin-bottom: 37px;
+      margin-bottom: 24px;
 
       .tag-big-title {
         position: relative;
@@ -117,21 +123,22 @@ export const tagsContainer = {
         height: 150px;
         background: rgba(255, 255, 255, 0.06);
         border-radius: 10px;
-        margin-bottom: 10px;
+        margin-bottom: 7px;
 
         .tag-big-title-name {
           box-sizing: border-box;
           flex-direction: row;
           align-items: flex-start;
-          padding: 7px 14px;
+          padding: 8px 14px 7px 10px;
+
           gap: 10px;
 
           position: absolute;
-          max-width: 129px;
+          max-width: 135px;
           height: 50px;
           left: 10px;
           top: 86px;
-
+          font-size: ${fontSize.xxLarge};
           border: 4px solid #ffffff;
           border-radius: 8px;
           text-overflow: ellipsis;
@@ -141,6 +148,7 @@ export const tagsContainer = {
       }
 
       .tag-sub-title {
+        position: relative;
         width: 143px;
         font-weight: ${fontWeight.lighter};
         line-height: 150%;
@@ -155,6 +163,8 @@ export const tagsContainer = {
         }
 
         .tag-sub-title-count {
+          position: absolute;
+          top: 20px;
           font-size: 11.175px;
           letter-spacing: 0.3725px;
           color: ${colors.textGray};
