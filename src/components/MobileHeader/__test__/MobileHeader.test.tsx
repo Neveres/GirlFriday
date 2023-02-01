6c36d576947e48dc89022c8dbfda18f3a2ef3c03
Leo <lechewu@gmail.com>
2023-02-01 20:19:55 +0800

    test(MobileHeader): add tests

--- src/components/MobileHeader/__test__/MobileHeader.test.tsx
+++ src/components/MobileHeader/__test__/MobileHeader.test.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { create } from 'react-test-renderer'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { MobileHeader } from 'src/components'
 
+const mockUseNavigate = jest.fn()
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useNavigate: () => jest.fn(),
+  useNavigate: () => mockUseNavigate,
 }))
 
 describe('MobileHeader', () => {
   test('should render well', () => {
     const testRenderer = create(<MobileHeader />)
     expect(testRenderer.toJSON()).toMatchSnapshot()
+  })
+
+  test('redirect should work well', async () => {
+    render(<MobileHeader />)
+    fireEvent.click(screen.getByTestId('KeyboardArrowLeftIcon'))
+    await waitFor(() => {
+      expect(mockUseNavigate).toBeCalled()
+    })
   })
 })
