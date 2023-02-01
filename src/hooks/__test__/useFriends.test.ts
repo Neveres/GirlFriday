b20d7a3391b080e5e20fdab80ea70eb66939f3ef
Leo <lechewu@gmail.com>
2023-02-01 19:24:28 +0800

    test(useFriends): add tests

---
+++ src/hooks/__test__/useFriends.test.ts
@@ -0,0 +1,66 @@
+import { waitFor, act } from '@testing-library/react'
+import { renderHook } from '@testing-library/react-hooks'
+import { useFriends } from 'src/hooks'
+
+const data = [
+  {
+    id: 'd3938d18-4e2a-4acf-906c-039e69747ad1',
+    name: 'Jason Senger',
+    username: 'Hertha43',
+    avater: 'https://cdn.fakercloud.com/avatars/adamawesomeface_128.jpg',
+    isFollowing: true,
+  },
+  {
+    id: '04e21ffe-9876-45d2-ad18-13223cbe4cd8',
+    name: 'Nellie Conroy',
+    username: 'Roosevelt32',
+    avater: 'https://cdn.fakercloud.com/avatars/swaplord_128.jpg',
+    isFollowing: true,
+  },
+]
+
+const mockResponse = {
+  data: {
+    data: [
+      {
+        ...data[0],
+      },
+    ],
+  },
+}
+jest.mock('src/libraries', () => ({
+  ...jest.requireActual('src/libraries'),
+  httpClient: { get: () => new Promise((resolve) => resolve(mockResponse)) },
+}))
+
+describe('useFriends', () => {
+  test('should return expected friends', async () => {
+    const { result } = renderHook(useFriends)
+
+    await waitFor(() => {
+      expect(result.current.friends).toStrictEqual([data[0]])
+    })
+  })
+
+  test('increasePage should work well', async () => {
+    const { result } = renderHook(useFriends)
+
+    await waitFor(() => {
+      expect(result.current.friends).toStrictEqual([data[0]])
+    })
+
+    mockResponse.data.data = [
+      {
+        ...data[1],
+      },
+    ]
+
+    act(() => {
+      result.current.increasePage()
+    })
+
+    await waitFor(() => {
+      expect(result.current.friends).toStrictEqual(data)
+    })
+  })
+})
